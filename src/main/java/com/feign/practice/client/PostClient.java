37d9583d030ef52e6b7227fc34446138b3501961
Firas <ferasama@gmail.com>
2021-01-31 09:43:22 +0800

    last edit

--- src/main/java/com/feign/practice/client/PostClient.java
+++ src/main/java/com/feign/practice/client/PostClient.java
@@ -3,9 +3,11 @@ package com.feign.practice.client;
 import java.util.List;
 
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 
 import com.feign.practice.dto.Post;
 
@@ -20,4 +22,10 @@ public interface PostClient {
 
 	@GetMapping("/posts/{id}")
 	public Post getById(@PathVariable Integer id);
+	
+	@PutMapping("/posts/{id}")
+	public Post editPost(@PathVariable Integer id, Post post);
+
+	@DeleteMapping("/posts/{id}")
+	public void deletePost(@PathVariable Integer id);
 }
--- src/main/java/com/feign/practice/controller/PostController.java
+++ src/main/java/com/feign/practice/controller/PostController.java
@@ -3,16 +3,16 @@ package com.feign.practice.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.feign.practice.client.PostClient;
 import com.feign.practice.dto.Post;
-
-import ch.qos.logback.core.net.server.Client;
 
 @RestController
 public class PostController {
@@ -33,5 +33,16 @@ public class PostController {
 	@GetMapping("/posts/{id}")
 	public Post getById(@PathVariable Integer id) {
 		return postClient.getById(id);
+	}
+	
+	@PutMapping("/posts/{id}")
+	public Post editPost(@PathVariable Integer id, @RequestBody Post post) {
+		return postClient.editPost(id, post);
+	}
+	
+	@DeleteMapping("/posts/{id}")
+	public String deletePost(@PathVariable Integer id) {
+		postClient.deletePost(id);
+		return "deleted";
 	}
 }
